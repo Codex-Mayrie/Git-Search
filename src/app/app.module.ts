9034e2b90aee83e1eb0f51592b4f4019114d5dc3
shee1998 <marywanjikumburu1@gmail.com>
2019-11-02 16:12:39 +0300

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Routes, RouterModule } from '@angular/router';
 import { GitSearchFormComponent } from './git-search-form/git-search-form.component';
 import { GitsearchComponent } from './gitsearch/gitsearch.component';
 import { HttpService } from './http-service';
+
+
+const routes: Routes = [
+  { path: 'navbar', component: NavBarComponent},
+
+];
 
 @NgModule({
   declarations: [
@@ -20,8 +27,8 @@ import { HttpService } from './http-service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
-
+    HttpClientModule,
+    [RouterModule.forRoot(routes)]
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
