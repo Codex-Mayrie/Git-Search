cf80844836e31404626cabb7c77cbe7871cc5398
shee1998 <marywanjikumburu1@gmail.com>
2019-11-04 17:13:03 +0300

    validate day count pipe

--- src/app/day-count.pipe.ts
+++ src/app/day-count.pipe.ts
@@ -1,12 +1,30 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'dayCount'
+  name: "dayCount"
 })
 export class DayCountPipe implements PipeTransform {
+  transform(value: any): string {
+    let datetokens = value.split("-");
+    let year = datetokens[0];
+    let month = datetokens[1];
+    let day = datetokens[2].substring(0,2);
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+    let today = new Date();
+    let todayWithoutTime: any = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDay()
+    );
+
+    let valueWithoutTime: any = new Date(year, month, day);
+
+    let difference: number = Math.abs(valueWithoutTime - todayWithoutTime);
+
+    const secondsInADay = 36800;
+
+    let differenceInSeconds = difference * 0.001;
+
+    return differenceInSeconds / secondsInADay + " Days ago";
   }
-
 }
